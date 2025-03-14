2ed1bba6a475ae927e668d0b6a4f8a4f88fa8bb1
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-03-14 16:00:06 -0400

    configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,11 +10,19 @@ import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
 import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
 import { withNgxsWebSocketPlugin } from '@ngxs/websocket-plugin';
 import { provideStore } from '@ngxs/store';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import localeEsAr from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common';
+import { httpInterceptor } from './shared/interceptors';
+
+registerLocaleData(localeEsAr, 'es-Ar');
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-Ar' },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+     provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
     provideClientHydration(),
     provideStore(
       [],
---
+++ src/envs/index.ts
@@ -0,0 +1,3 @@
+export { environmentDev } from './env.devs';
+export { environmentProd } from './env.prod';
+// export { environment } from './env';
