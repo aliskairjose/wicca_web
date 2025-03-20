f3cd3401151ff89f4be2ac16a823be3bd2ec7563
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-03-19 20:54:35 -0400

    login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngxs/store';
 import { ButtonComponent, InputComponent } from '@shared/components';
+import { ToastService } from '@shared/services';
+import { AuthActions } from '../store/auth.actions';
+import { MessageEnum } from '@shared/enums';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+    form!: FormGroup;
+
+  #fb = inject(FormBuilder);
+  #store = inject(Store);
+  #router = inject(Router);
+  #toastService = inject(ToastService);
+
+  ngOnInit(): void {
+    this._loadForm();
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.form.valid && this._login(this.form.value);
+  }
+
+  private _login(data: any): void {
+    this.#store.dispatch(new AuthActions.Login(data)).subscribe(() => {
+      this.#toastService.show(MessageEnum.Welcome);
+      // setTimeout(() => {
+      //   this.#router.navigate([RoutesEnum.Dashboard]);
+      // }, 100);
+    });
+  }
+
+  private _loadForm(): void {
+    this.form = this.#fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 }
