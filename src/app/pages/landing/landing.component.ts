0926183c77097ce60404b693c36d36b739c0b862
Kervin Gonzalez <kervingonzalez@gmail.com>
2025-09-03 13:07:29 -0400

    landng

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavbarComponent } from '@shared/components';
 import { FooterComponent } from "@shared/components/footer/footer.component";
+import { TopRatedInterface } from '../dashboard/home/interfaces/top-rated.interface';
+import { HomeAction } from '../dashboard/home/store/home.actions';
+import { Store } from '@ngxs/store';
+import { firstValueFrom } from 'rxjs';
+import { HomeSelectors } from '../dashboard/home/store/home.selectors';
+import { UserInterface } from '../dashboard/users/user.interface';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
+  topRatedAdvisors: TopRatedInterface[] = [];
+  newUsers: UserInterface[] = [];
+  #store = inject(Store);
+
+  ngOnInit(): void {
+    this._loadData();
+  }
+
+  private async _loadData() {
+    await Promise.all([
+      firstValueFrom(this.#store.dispatch(new HomeAction.GetNewRegistrations)),
+      firstValueFrom(this.#store.dispatch(new HomeAction.GetTopAdvisors)),
+    ]);
+    this.newUsers = this.#store.selectSnapshot(HomeSelectors.newRegistrations);
+    this.topRatedAdvisors = this.#store.selectSnapshot(HomeSelectors.topRatedAdvisors);
+
+  }
 }
