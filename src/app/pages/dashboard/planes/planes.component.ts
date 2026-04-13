0395d59c618a115e43ce62df2ace5f9b5c4c16d4
deepcompanykg <frontend.kg@deepcompany.com>
2026-04-12 21:01:15 -0400

    delete plan

--- src/app/pages/dashboard/planes/planes.component.ts
+++ src/app/pages/dashboard/planes/planes.component.ts
@@ -1,18 +1,41 @@
-import { Component, Inject, inject, OnInit, signal, OnChanges } from '@angular/core';
+import {
+  Component,
+  Inject,
+  inject,
+  OnInit,
+  signal,
+  OnChanges,
+} from '@angular/core';
 import { PlanesService } from './planes.service';
 import { PlanInterface } from './interfaces/plan.interface';
-import { CommonModule, CurrencyPipe, DatePipe, DOCUMENT } from '@angular/common';
+import {
+  CommonModule,
+  CurrencyPipe,
+  DatePipe,
+  DOCUMENT,
+} from '@angular/common';
 import { HSOverlay } from 'flyonui/flyonui';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { BadgeComponent, InputComponent, SwitchComponent } from '@shared/components';
-
+import {
+  BadgeComponent,
+  InputComponent,
+  SwitchComponent,
+} from '@shared/components';
 
 @Component({
   selector: 'app-planes',
   standalone: true,
-  imports: [DatePipe, CurrencyPipe, CommonModule, ReactiveFormsModule, InputComponent, SwitchComponent, BadgeComponent],
+  imports: [
+    DatePipe,
+    CurrencyPipe,
+    CommonModule,
+    ReactiveFormsModule,
+    InputComponent,
+    SwitchComponent,
+    BadgeComponent,
+  ],
   templateUrl: './planes.component.html',
-  styleUrl: './planes.component.scss'
+  styleUrl: './planes.component.scss',
 })
 export class PlanesComponent implements OnInit {
   form!: FormGroup;
@@ -26,14 +49,12 @@ export class PlanesComponent implements OnInit {
   planes: PlanInterface[] = [];
   plan = signal<PlanInterface | undefined>(undefined);
 
-  constructor(
-    @Inject(DOCUMENT) private document: Document
-  ) {
+  constructor(@Inject(DOCUMENT) private document: Document) {
     this.form = this.#fb.group({
       amount: [''],
       bonus: [''],
-      isActive: ['']
-    })
+      isActive: [''],
+    });
   }
 
   get f() {
@@ -44,21 +65,31 @@ export class PlanesComponent implements OnInit {
     this._loadData();
   }
 
-  openModal(isEdit = false, index = 0): void {
+  openDeleteModal(plan: PlanInterface): void {
+    this.plan.set(plan);
+    const modal = new HSOverlay(this.document.querySelector('#delete-modal')!);
+    modal.open();
+  }
+
+  closeDeleteModal(): void {
+    const modal = new HSOverlay(this.document.querySelector('#delete-modal')!);
+    modal.close();
+  }
+
+  openModal(isEdit = false, plan?: PlanInterface | undefined): void {
     this.isEdit.set(isEdit);
 
     if (isEdit) {
-      this.plan.set(this.planes[index]);
-
+      this.plan.set(plan);
       this.form.patchValue({
         amount: this.plan()!.amount,
         bonus: this.plan()!.bonus,
-        isActive: this.plan()!.isActive
+        isActive: this.plan()!.isActive,
       });
     }
 
     const modal = new HSOverlay(this.document.querySelector('#plan-modal')!);
-    modal.open()
+    modal.open();
   }
 
   closeModal(): void {
@@ -68,13 +99,11 @@ export class PlanesComponent implements OnInit {
   }
 
   onChange(event: boolean): void {
-    this.form.patchValue({ isActive: event })
+    this.form.patchValue({ isActive: event });
   }
 
   onSubmit(): void {
-    (this.isEdit())
-      ? this._update()
-      : this._create();
+    this.isEdit() ? this._update() : this._create();
   }
 
   onDelete(): void {
@@ -84,27 +113,25 @@ export class PlanesComponent implements OnInit {
   private _create(): void {
     this.#service.create(this.form.value).subscribe(() => {
       this.closeModal();
-      this._loadData()
+      this._loadData();
     });
   }
 
   private _update(): void {
     this.#service.update(this.plan()!._id, this.form.value).subscribe(() => {
       this.closeModal();
-      this._loadData()
+      this._loadData();
     });
   }
 
   private _delete(): void {
     this.#service.delete(this.plan()!._id).subscribe(() => {
-      this.closeModal();
+      this.closeDeleteModal();
       this._loadData();
-    })
+    });
   }
 
   private _loadData(): void {
-    this.#service.list().subscribe((planes) => this.planes = planes);
+    this.#service.list().subscribe((planes) => (this.planes = planes));
   }
-
-
 }
