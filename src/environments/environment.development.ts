8377efbbc2dec72793b2709c43790f2d96e84b30
Kervin Gonzalez <kervingonzalez@gmail.com>
2026-08-17 18:15:43 -0400

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://4szh2pd3-3000.use2.devtunnels.ms/v1',
-  socket: 'https://4szh2pd3-3001.use2.devtunnels.ms'
+  baseUrl: 'https://localhost:3000/v1',
+  socket: 'https://localhost:3001'
 };
